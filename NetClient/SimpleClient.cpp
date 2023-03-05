c7e8475ac931eeb53a406488983bfad3196192ee
quangnd123 <vinhquan001@e.ntu.edu.sg>
2023-03-05 21:14:32 +0800

    first commit

---
+++ NetClient/SimpleClient.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <olc_net.h>
+
+enum class CustomMsgTypes : uint32_t
+{
+	ServerAccept,
+	ServerDeny,
+	ServerPing,
+	MessageAll,
+	ServerMessage,
+};
+
+
+
+class CustomClient : public olc::net::client_interface<CustomMsgTypes>
+{
+public:
+	void PingServer()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ServerPing;
+
+		// Caution with this...
+		std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+
+		msg << timeNow;
+		Send(msg);
+	}
+
+	void MessageAll()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::MessageAll;
+		Send(msg);
+	}
+};
+
+int main()
+{
+	CustomClient c;
+	c.Connect("127.0.0.1", 60000);
+
+	bool key[3] = { false, false, false };
+	bool old_key[3] = { false, false, false };
+
+	bool bQuit = false;
+	while (!bQuit)
+	{
+		//if (GetForegroundWindow() == GetConsoleWindow())
+		if(true)
+		{
+			//up arrow
+			key[0] = GetAsyncKeyState(VK_UP) & 0x8000;
+			//down arrow
+			key[1] = GetAsyncKeyState(VK_DOWN) & 0x8000;
+			//ESC
+			key[2] = GetAsyncKeyState(VK_ESCAPE) & 0x8000;
+		}
+		if (key[0] && !old_key[0]) c.PingServer();
+		if (key[1] && !old_key[1]) c.MessageAll();
+		if (key[2] && !old_key[2]) bQuit = true;
+		for (int i = 0; i < 3; i++) old_key[i] = key[i];
+		
+
+		if (c.IsConnected())
+		{
+			if (!c.Incoming().empty())
+			{
+
+
+				auto msg = c.Incoming().pop_front().msg;
+
+				switch (msg.header.id)
+				{
+				case CustomMsgTypes::ServerAccept:
+				{
+					// Server has responded to a ping request				
+					std::cout << "Server Accepted Connection\n";
+				}
+				break;
+
+
+				case CustomMsgTypes::ServerPing:
+				{
+					// Server has responded to a ping request
+					std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+					std::chrono::system_clock::time_point timeThen;
+					msg >> timeThen;
+					std::cout << "Ping: " << std::chrono::duration<double>(timeNow - timeThen).count() << "\n";
+				}
+				break;
+
+				case CustomMsgTypes::ServerMessage:
+				{
+					// Server has responded to a ping request	
+					uint32_t clientID;
+					msg >> clientID;
+					std::cout << "Hello from [" << clientID << "]\n";
+				}
+				break;
+				}
+			}
+		}
+		else
+		{
+			std::cout << "Server Down\n";
+			bQuit = true;
+		}
+
+		//int in;
+		//std::cout << "Give command: \n";
+		//std::cin >> in;
+		//if (in == 1) c.PingServer();
+		//else if (in == 2) c.MessageAll();
+		//else bQuit = true;
+	}
+
+	return 0;
+}
